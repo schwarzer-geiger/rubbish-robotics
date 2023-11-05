291428d0ddaf93e1090538018030aad34725fbe7
schwarzer-geiger <lalitmistry1407@gmail.com>
2023-11-05 10:30:14 +0000

    Fixed moveToXY multistepper problem

--- v1code/v1code.ino
+++ v1code/v1code.ino
@@ -1,5 +1,5 @@
 #include <Encoder.h>
-#include "AccelStepper.h"
+#include <AccelStepper.h>
 #include <MultiStepper.h>
 
 // Macros
@@ -23,226 +23,226 @@ float L2 = 10;
 
 // The motor class acts as an interface implemented by dc and stepper.
 class motor {
-  public:
-    virtual void callNSteps(int, int) {
-    }
+public:
+  virtual void callNSteps(int, int) {
+  }
 
-    virtual void getAngle(int, int) {
-    }
+  virtual void getAngle(int, int) {
+  }
 
-    virtual void moveToAngle(int) {
-    }
+  virtual void moveToAngle(int) {
+  }
 
-    float stepsPerDeg;
+  float stepsPerDeg;
 
-  protected:
-    // Helper function, contains all the zero'ing code compatible with both motor types
-    void manualMov() {
+protected:
+  // Helper function, contains all the zero'ing code compatible with both motor types
+  void manualMov() {
 
-      // executes zeroing process
-      Serial.println("Please use the following command format to move the arm.");
-      Serial.println("               +0120           ");
-      Serial.println("            steps ^");
-      Serial.println("Once the desired position has been achieved, type 'alldone'");
+    // executes zeroing process
+    Serial.println("Please use the following command format to move the arm.");
+    Serial.println("               +0120           ");
+    Serial.println("            steps ^");
+    Serial.println("Once the desired position has been achieved, type 'alldone'");
 
-      bool isDone = false;
-      char inputs[6] = "00000";
+    bool isDone = false;
+    char inputs[6] = "00000";
 
-      while (!isDone) {
-        // to prevent risk of broken data, only read if there are at least 7 characters
-        if (Serial.available() >= 5) {
-          int steps;
-          int dir;
-          int index;
-          for (int i = 0; i < 5; i++) {
-            inputs[i] = Serial.read();
-          }
-
-          // discard anything after the first 5 characters just in case
-          while (Serial.available()) {
-            Serial.read();
-          }
-
-          // TODO: Check this with new input format, don't understand
-          if (inputs[0] == '+' || inputs[0] == '-') {
-            steps = (inputs[1] - '0') * 1000 + (inputs[2] - '0') * 100 + (inputs[3] - '0') * 10 + (inputs[4] - '0');
-            dir = !(inputs[0] == '+');  // CW (1) if +, CCW (2) if -
-            // not needed anymore? index = inputs[6] - '0';
-
-            Serial.print("Set steps ");
-            Serial.print(steps);
-            Serial.print(" to index ");
-            Serial.println(index);
-
-            callNSteps(steps, dir);
-          }
-          // quickly checking if inputs is alldone by just comparing the first letter
-          if (inputs[0] == 'a')
-            isDone = true;
+    while (!isDone) {
+      // to prevent risk of broken data, only read if there are at least 7 characters
+      if (Serial.available() >= 5) {
+        int steps;
+        int dir;
+        int index;
+        for (int i = 0; i < 5; i++) {
+          inputs[i] = Serial.read();
         }
+
+        // discard anything after the first 5 characters just in case
+        while (Serial.available()) {
+          Serial.read();
+        }
+
+        // TODO: Check this with new input format, don't understand
+        if (inputs[0] == '+' || inputs[0] == '-') {
+          steps = (inputs[1] - '0') * 1000 + (inputs[2] - '0') * 100 + (inputs[3] - '0') * 10 + (inputs[4] - '0');
+          dir = !(inputs[0] == '+');  // CW (1) if +, CCW (2) if -
+          // not needed anymore? index = inputs[6] - '0';
+
+          Serial.print("Set steps ");
+          Serial.print(steps);
+          Serial.print(" to index ");
+          Serial.println(index);
+
+          callNSteps(steps, dir);
+        }
+        // quickly checking if inputs is alldone by just comparing the first letter
+        if (inputs[0] == 'a')
+          isDone = true;
       }
     }
+  }
 };
 
 // A dc object consists of a motor and an encoder object, and a position variable to track the motor position
 // Clockwise (CW) rotation increases the position value.
 
 class dc : public motor {
-  public:
-    // constructor
-    dc(int powerPin1, int powerPin2, int enc1Pin, int enc2Pin)
-      : powerPin1(powerPin1), powerPin2(powerPin2), enc(enc1Pin, enc2Pin) {
+public:
+  // constructor
+  dc(int powerPin1, int powerPin2, int enc1Pin, int enc2Pin)
+    : powerPin1(powerPin1), powerPin2(powerPin2), enc(enc1Pin, enc2Pin) {
+  }
+
+  void callNSteps(int steps, int dir) {
+    moveNSteps(steps, dir, DEFAULT_SPEED_DC);
+  }
+
+  // returns the angle of the robot arm in degrees [see drawing]
+  float getAngle() {
+    return (float)position / stepsPerDeg;
+  }
+
+  // moves motor/arm to desired angle at desired speed
+  void moveToAngle(float targetAngle, int speed) {
+    float currentAngle = getAngle();
+    float difference = targetAngle - currentAngle;
+    int stepsToTurn = (int)abs(difference * stepsPerDeg);
+    int dir;
+    if (difference > 0) {
+      dir = CW;
+    } else {
+      dir = CCW;
+    }
+    Serial.print("here6: ");
+    Serial.println(stepsToTurn);
+    moveNSteps(stepsToTurn, dir, speed);
+  }
+
+  // Moves motor by nSteps encoder steps into direction dir (CW or CCW) at speed 'speed'.
+  void moveNSteps(int nSteps, int dir, int speed) {
+    // start with motor off
+    analogWrite(powerPin1, 0);
+    analogWrite(powerPin2, 0);
+
+    int lastSignal = enc.read();
+    int currentSignal;
+    // steps run during the current function call, direction not considered
+    int stepsRun = 0;
+    int initPosition = position;
+    int targetPosition;
+
+    // calculate target position
+    if (dir == CW) {
+      targetPosition = initPosition + nSteps;
+    } else {
+      targetPosition = initPosition - nSteps;
     }
 
-    void callNSteps(int steps, int dir) {
-      moveNSteps(steps, dir, DEFAULT_SPEED_DC);
+    // start motor
+    // speed requires: between 0 and 255
+    if (dir == CW) {
+      analogWrite(powerPin1, speed);
+    } else {
+      analogWrite(powerPin2, speed);
     }
 
-    // returns the angle of the robot arm in degrees [see drawing]
-    float getAngle() {
-      return (float) position / stepsPerDeg;
-    }
-
-    // moves motor/arm to desired angle at desired speed
-    void moveToAngle(float targetAngle, int speed) {
-      float currentAngle = getAngle();
-      float difference = targetAngle - currentAngle;
-      int stepsToTurn = (int) abs(difference * stepsPerDeg);
-      int dir;
-      if (difference > 0) {
-        dir = CW;
-      } else {
-        dir = CCW;
-      }
-      Serial.print("here6: ");
-      Serial.println(stepsToTurn);
-      moveNSteps(stepsToTurn, dir, speed);
-    }
-
-    // Moves motor by nSteps encoder steps into direction dir (CW or CCW) at speed 'speed'.
-    void moveNSteps(int nSteps, int dir, int speed) {
-      // start with motor off
-      analogWrite(powerPin1, 0);
-      analogWrite(powerPin2, 0);
-
-      int lastSignal = enc.read();
-      int currentSignal;
-      // steps run during the current function call, direction not considered
-      int stepsRun = 0;
-      int initPosition = position;
-      int targetPosition;
-
-      // calculate target position
-      if (dir == CW) {
-        targetPosition = initPosition + nSteps;
-      } else {
-        targetPosition = initPosition - nSteps;
-      }
-
-      // start motor
-      // speed requires: between 0 and 255
-      if (dir == CW) {
-        analogWrite(powerPin1, speed);
-      } else {
-        analogWrite(powerPin2, speed);
-      }
-
-      // check whether the number of encoder steps passed 'stepsRun' has reached the desired number of steps 'nSteps' continuously
-      while (stepsRun < nSteps) {
-        currentSignal = enc.read();
-        if (currentSignal != lastSignal) {
-          stepsRun++;
-          lastSignal = currentSignal;
-        }
-      }
-
-      analogWrite(powerPin1, 0);
-      analogWrite(powerPin2, 0);
-      enc.write(0);
-      Serial.println("Motor stopped");
-
-      if (dir == CW) {
-        position += stepsRun;
-      } else {
-        position -= stepsRun;
-      }
-
-      if (SERIAL_PRINT) {
-        Serial.print("Initial position: ");
-        Serial.println(initPosition);
-        Serial.print("Target Position: ");
-        Serial.println(targetPosition);
-        Serial.print("Actual position: ");
-        Serial.println(position);
+    // check whether the number of encoder steps passed 'stepsRun' has reached the desired number of steps 'nSteps' continuously
+    while (stepsRun < nSteps) {
+      currentSignal = enc.read();
+      if (currentSignal != lastSignal) {
+        stepsRun++;
+        lastSignal = currentSignal;
       }
     }
-    
-    void zero() {
-      manualMov();
-      enc.write(0);
-      position = 0;
-      Serial.println("This arm successfully zero'd!");
+
+    analogWrite(powerPin1, 0);
+    analogWrite(powerPin2, 0);
+    enc.write(0);
+    Serial.println("Motor stopped");
+
+    if (dir == CW) {
+      position += stepsRun;
+    } else {
+      position -= stepsRun;
     }
 
-    void calibrate() {
-      manualMov();
-      stepsPerDeg = abs(position / CALIB_ANGLE);
-      enc.write(0);
+    if (SERIAL_PRINT) {
+      Serial.print("Initial position: ");
+      Serial.println(initPosition);
+      Serial.print("Target Position: ");
+      Serial.println(targetPosition);
+      Serial.print("Actual position: ");
+      Serial.println(position);
     }
+  }
 
-    int powerPin1;
-    int powerPin2;
-    Encoder enc;
-    int position = 0;
+  void zero() {
+    manualMov();
+    enc.write(0);
+    position = 0;
+    Serial.println("This arm successfully zero'd!");
+  }
+
+  void calibrate() {
+    manualMov();
+    stepsPerDeg = abs(position / CALIB_ANGLE);
+    enc.write(0);
+  }
+
+  int powerPin1;
+  int powerPin2;
+  Encoder enc;
+  int position = 0;
 };
 
 // A stepper object is defined by its dirPin which sets the pin that controls the direction of rotation (HIGH/LOW) and a stepPin.
 // Setting the stepPin HIGH and then LOW defines one step movement.
 class stepper : public motor {
-  public:
-    stepper(int stepPin, int dirPin)
-      : driver(1, stepPin, dirPin) {
-    }
+public:
+  stepper(int stepPin, int dirPin)
+    : driver(1, stepPin, dirPin) {
+  }
 
-    void callNSteps(int steps, int dir) {
-      moveNSteps(steps, dir, DEFAULT_ACCEL_STEPPER);
-    }
+  void callNSteps(int steps, int dir) {
+    moveNSteps(steps, dir, DEFAULT_ACCEL_STEPPER);
+  }
 
-    void moveNSteps(int nSteps, int dir, int accel) {
-      if (dir == CW) {
-        driver.move(nSteps);
-      } else {
-        driver.move(-nSteps);
-      }
-      driver.setAcceleration(accel);
-      driver.run();
+  void moveNSteps(int nSteps, int dir, int accel) {
+    if (dir == CW) {
+      driver.move(nSteps);
+    } else {
+      driver.move(-nSteps);
     }
+    driver.setAcceleration(accel);
+    driver.run();
+  }
 
-    // returns the angle of the robot arm in degrees [see drawing]
-    float getAngle() {
-      return (float) driver.currentPosition() / stepsPerDeg;
-    }
+  // returns the angle of the robot arm in degrees [see drawing]
+  float getAngle() {
+    return (float)driver.currentPosition() / stepsPerDeg;
+  }
 
-    // moves motor/arm to desired angle at desired acceleration
-    void moveToAngle(float targetAngle) {
-      int targetPosition = (int) targetAngle * stepsPerDeg;
-      driver.moveTo(targetPosition);
-    }
-    
-    void calibrate() {
-      manualMov();
-      stepsPerDeg = abs(driver.currentPosition() / CALIB_ANGLE);
-      Serial.println("This arm successfully calibrated!");
-    }
+  // moves motor/arm to desired angle at desired acceleration
+  void moveToAngle(float targetAngle) {
+    int targetPosition = (int)targetAngle * stepsPerDeg;
+    driver.moveTo(targetPosition);
+  }
 
-    AccelStepper driver;
+  void calibrate() {
+    manualMov();
+    stepsPerDeg = abs(driver.currentPosition() / CALIB_ANGLE);
+    Serial.println("This arm successfully calibrated!");
+  }
+
+  AccelStepper driver;
 };
 
 // Returns -1 if xTarget and/or yTarget are out of range of motion
 // Returns -2 if robot didn't manage to move to (xTarget, yTarget)
 // NOTE: IF THESE ARE DC INSTEAD OF MOTOR, IT USES THE PLACEHOLDER MOVENSTEPS CLASS
 // AND IT DOESNT DRIVE THE MOTORS!
-int moveToXY(motor m1, motor m2, float xTarget, float yTarget) {
+int moveToXY(MultiStepper steppers, motor m1, motor m2, float xTarget, float yTarget) {
 
   float D = sqrt(pow(xTarget, 2) + pow(yTarget, 2));
   Serial.print("here1:");
@@ -273,24 +273,31 @@ int moveToXY(motor m1, motor m2, float xTarget, float yTarget) {
   m1.moveToAngle(theta1Target * 180 / PI);
   m2.moveToAngle(theta2Target * 180 / PI);
 
+  long positions[2];
+  positions[0] = theta1Target * 180 / PI * m1.stepsPerDeg;
+  positions[1] = theta2Target * 180 / PI * m2.stepsPerDeg;
+
+  steppers.moveTo(positions);
+  steppers.runSpeedToPosition();
+
   return 0;
 }
 
 void setup() {
 
   // create stepper motors specifying direction pin, step pin and calibration reference angle.
-  stepper motor1(6, 5);
-  stepper motor2(9, 8);
+stepper motor1(6, 5);
+stepper motor2(9, 8);
 
-  MultiStepper steppers;
+MultiStepper steppers;
 
-  int SPEED = 80;
-  motor1.driver.setMaxSpeed(SPEED);
-  motor1.driver.setAcceleration(20);
-  motor2.driver.setMaxSpeed(SPEED);
-  motor2.driver.setAcceleration(20);
-  steppers.addStepper(motor1.driver);
-  steppers.addStepper(motor2.driver);
+int SPEED = 80;
+motor1.driver.setMaxSpeed(SPEED);
+motor1.driver.setAcceleration(20);
+motor2.driver.setMaxSpeed(SPEED);
+motor2.driver.setAcceleration(20);
+steppers.addStepper(motor1.driver);
+steppers.addStepper(motor2.driver);
 
   // Serial.begin(9600);
   // Serial.println("Please position the robot arm so that its lower arm is vertical and its upper arm is horizontal.");
@@ -304,8 +311,8 @@ void setup() {
   // Serial.println("You can now move motor 2.");
   // motor2.calibrate();
   Serial.println("Motor calibration done. Proceeding with IK!");
-  
-  moveToXY(motor1, motor2, 12, 6);
+
+  moveToXY(steppers, motor1, motor2, 12, 6);
   steppers.runSpeedToPosition();
 
   // while (true) {
